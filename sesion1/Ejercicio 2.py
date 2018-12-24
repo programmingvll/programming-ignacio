ec984a035bfaa3ba447c6223e42a6283e2c244f8
inadomin <46073323+inadomin@users.noreply.github.com>
2018-12-24 12:04:10 +0100

    Ejercicio 2

--- sesion1/Ejercicio 2.py
+++ sesion1/Ejercicio 2.py
@@ -1,3 +1,24 @@
 '''Escribir un programa Python que calcule la rentabilidad de un depósito anual de 234.000 € al 4.5%
 y la muestre en pantalla.
 '''
+
+Depositoanual=int(input(" depósito anual "))
+
+Interesanual=float(input(" interes anual "))
+
+Interesesanuales= int(Depositoanual*Interesanual)
+
+print( " Intereses en un año", Interesesanuales)
+
+Capitalfinal=int(Interesesanuales+Depositoanual)
+
+print("Capital acumulado al final del año con los intereses", Capitalfinal)
+
+Rentabilidadanual=(Capitalfinal-Depositoanual)/Depositoanual
+
+print("Rentabilidad anual TIR", Rentabilidadanual)
+
+
+''' Me ha llevado tiempo meter el decimal como float, pero como se resolvia
+el ejercicio ha sido fácil de plantear'''
+
